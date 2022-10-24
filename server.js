65b7fe806d57e32df7e0ad2ef8933798749e08ee
Christian Rangel <budrangel@gmail.com>
2022-10-24 14:03:04 -0700

    dfsd

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html/'))
 })
 app.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/dashboard.html/'))
+    res.sendFile(path.join(__dirname, './dashboard.html/'))
 })
 app.get('/entry', (req, res) => {
     res.sendFile(path.join(__dirname, './public/entry.html/'))
