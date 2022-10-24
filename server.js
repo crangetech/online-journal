337a49634d9a97e2bd79940f186d95a842161906
Christian Rangel <budrangel@gmail.com>
2022-10-24 13:45:16 -0700

    created public folder

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use("/api",userRoutes);
 //
 // app.use(cors());
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html/'))
+    res.sendFile(path.join(__dirname, '/public/index.html/'))
 })
 app.get('/dashboard', (req, res) => {
     res.sendFile(path.join(__dirname, '/dashboard.html/'))
